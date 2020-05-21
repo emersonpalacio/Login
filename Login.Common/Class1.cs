4f57ded871836ab60ddf2df6044177b979f48b9c
emersonpalacio <emersonpalaciootalvaro@gmail.com>
2020-05-21 18:22:46 -0500

    FirstDatabase

---
+++ Login.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Login.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ Login.Web/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Login.Web.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Login.Web.Data
+{
+    public class DataContext :DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<TaxiEntity> Taxis { get; set; }
+    }
+}
